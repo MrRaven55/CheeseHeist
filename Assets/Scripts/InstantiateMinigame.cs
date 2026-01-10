c27fdb5e2fecaa9ec11338045e67d01154b54b41
Arthur <lotuslovesurmom@gmail.com>
2026-01-10 14:27:32 +0100

    Add InstantiateMinigame script
    Created a new Unity MonoBehaviour script with serialized fields for three minigame prefabs. The script currently contains empty Start and Update methods for future logic.

---
+++ Assets/Scripts/InstantiateMinigame.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateMinigame : MonoBehaviour
+{
+
+    [SerializeField] private GameObject minigamePrefab1;
+    [SerializeField] private GameObject minigamePrefab2;
+    [SerializeField] private GameObject minigamePrefab3;
+
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+
+    }
+}
