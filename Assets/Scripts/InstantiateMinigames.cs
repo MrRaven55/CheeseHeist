0d3d4fdf5572d02b6d369685953c53ba63dbed20
Arthur <lotuslovesurmom@gmail.com>
2026-01-14 16:19:49 +0100

    Add two minigames and supporting prefabs to scene
    Introduced Minigame1 and Minigame2 with their respective UI elements and logic scripts. Added FruitPrefab and related meta files for Minigame2. Updated Arthur.unity to include new minigame canvases, objects, and event system. Modified InstantiateMinigames to support minigame start events.

--- Assets/Scripts/InstantiateMinigames.cs
+++ Assets/Scripts/InstantiateMinigames.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InstantiateMinigames : MonoBehaviour
 {
-
+    public static event Action<float>  startMinigame;
     void Start()
     {
         
@@ -23,7 +24,7 @@ public class InstantiateMinigames : MonoBehaviour
             if (other.CompareTag("Player"))
             {
                 // for Oscar: Get player ID and change camera instantied minigame accordingly 
-
+                // Call action event to start minigame(float is cam type)
                 // Get component from parent gameprefab identifier
             }
         }
