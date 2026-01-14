43d8438f9b4b3f91e75b70e4c772bc02cc4c3d81
GoatDaBird <victor.jeppsson@elev.ga.lbs.se>
2026-01-14 22:37:20 +0100

    current progress

---
+++ Assets/Scripts/Passive gain (in progress)/ForestWorkerBoard.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestWorkerBoard : MonoBehaviour, IInteractable
+{
+    public KeyCode upButton;
+
+
+    public string woodType; //pine, birch or oak
+    public PassiveWoodGain passiveWoodGain;
+
+    private bool uiOpen = false;
+
+    public void Interact(int playerID)
+    {
+        //open UI
+        uiOpen = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiOpen)
+        {
+        }
+    }
+}
