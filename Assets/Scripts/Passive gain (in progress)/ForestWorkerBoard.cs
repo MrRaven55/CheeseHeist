a1622a13b7982add0fbf50a708bc0186e6930f4d
GoatDaBird <victor.jeppsson@elev.ga.lbs.se>
2026-04-22 10:41:25 +0200

    Imported grahics

--- Assets/Scripts/Passive gain (in progress)/ForestWorkerBoard.cs
+++ Assets/Scripts/Passive gain (in progress)/ForestWorkerBoard.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -67,3 +67,8 @@ public class ForestWorkerBoard : MonoBehaviour, IInteractable
         }
     }
 }
+
+
+
+
+*/
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -31,10 +31,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!isInMenu)
-        {
-            Movement();
-        }
+        Movement();
     }
 
     /// <summary>
